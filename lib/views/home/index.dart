82cb60508e07414159b8c612b66977225c8ed9fb
Jose Teixeira <josejonicaro@gmail.com>
2022-08-18 21:50:41 -0300

    Cores alteradas

--- lib/views/home/index.dart
+++ lib/views/home/index.dart
@@ -17,149 +17,271 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white.withOpacity(0.9),
-      body: ListView(
-        children: [
-          const LogoWidget(
-            topPadding: 24,
+    return Container(
+      color: GlobalInfo.secondaryColor,
+      child: SafeArea(
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: ListView(
+            children: const [BoxOne(), BoxTwo()],
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 24, top: 32),
-            child: GestureDetector(
-              onHorizontalDragEnd: (details) => print('test'),
-              child: Container(
-                height: 120,
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 5,
-                      offset: Offset(0, 5),
-                      color: Colors.grey,
-                    ),
-                  ],
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    bottomLeft: Radius.circular(15),
-                  ),
-                  color: GlobalInfo.tertiaryColor,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 5, bottom: 5, left: 5),
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        bottomLeft: Radius.circular(10),
+        ),
+      ),
+    );
+  }
+}
+
+class BoxOne extends StatelessWidget {
+  const BoxOne({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      color: GlobalInfo.primaryColor,
+      child: IntrinsicHeight(
+        child: Column(
+          children: [
+            const LogoWidget(
+              topPadding: 24,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 24, top: 32),
+              child: GestureDetector(
+                onHorizontalDragEnd: (details) => print('test'),
+                child: Container(
+                  height: 120,
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 5,
+                        offset: Offset(0, 5),
+                        color: Colors.white10,
                       ),
-                      color: Colors.white,
+                    ],
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
                     ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: Icon(
-                            Icons.drag_indicator_rounded,
-                            color: Colors.grey.withOpacity(0.5),
-                          ),
+                    color: GlobalInfo.tertiaryColor,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 5, bottom: 5, left: 5),
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 8.0, horizontal: 8),
-                          child: Container(
-                            height: 75,
-                            width: 56,
-                            decoration: const BoxDecoration(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10)),
-                                image: DecorationImage(
-                                    fit: BoxFit.cover,
-                                    image: AssetImage(
-                                        'assets/images/avatar.png'))),
+                        color: Colors.white,
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Icon(
+                              Icons.drag_indicator_rounded,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
                           ),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const [
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                'Nome do Usuário',
-                                style: TextStyle(
-                                    color: GlobalInfo.contrastColor,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 8),
+                            child: Container(
+                              height: 75,
+                              width: 56,
+                              decoration: const BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10)),
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          'assets/images/avatar.png'))),
                             ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                'arreste para mais informações',
-                                style:
-                                    TextStyle(color: GlobalInfo.contrastColor),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: const [
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Nome do Usuário',
+                                  style: TextStyle(
+                                      color: GlobalInfo.contrastColor,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16),
+                                ),
                               ),
-                            ),
-                          ],
-                        )
-                      ],
+                              Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  'arreste para mais informações',
+                                  style: TextStyle(
+                                      color: GlobalInfo.contrastColor),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('28 / OUT'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BoxTwo extends StatefulWidget {
+  const BoxTwo({Key? key}) : super(key: key);
+
+  @override
+  State<BoxTwo> createState() => _BoxTwoState();
+}
+
+class _BoxTwoState extends State<BoxTwo> {
+  bool _visibleList = false;
+  String _textButton = 'Ver mais';
+  IconData _iconButton = Icons.expand_more_rounded;
+
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      // height: ,
+      padding: const EdgeInsets.all(8),
+      color: Colors.white,
+      child: IntrinsicHeight(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: GestureDetector(
+                onTap: () => print('alterar data'),
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Registrar Atividades do Dia',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: GlobalInfo.contrastColor,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        '28 / OUT',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: GlobalInfo.contrastColor,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.edit_calendar_rounded,
+                      size: 18,
+                      color: GlobalInfo.grey,
+                    )
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text('Ativiades do dia: '),
-                ),
-              ],
+              ),
             ),
-          ),
-          SizedBox(
-            height: 100,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: ListView(
-                // controller: _scrollController,
-                scrollDirection: Axis.horizontal,
-                // physics: const ClampingScrollPhysics(),
-                children: const [
-                  ActionsWidget(
-                    name: 'Registrar Crise',
-                    activities: ['horario', 'instensidade'],
+            Visibility(
+              visible: _visibleList,
+              child: SizedBox(
+                height: 5 * 90,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    // controller: _scrollController,
+                    // shrinkWrap: true,
+                    // physics: const ClampingScrollPhysics(),
+                    children: const [
+                      ActionsWidget(
+                        name: 'Registrar Crise',
+                        activities: ['horario', 'instensidade'],
+                        icon: Icons.book_online_rounded,
+                      ),
+                      ActionsWidget(
+                        name: 'Registrar Alimentação',
+                        activities: ['horario', 'alimentos'],
+                        icon: Icons.restaurant_rounded,
+                      ),
+                      ActionsWidget(
+                        name: 'Registrar Atividades',
+                        activities: ['horario', 'atividade'],
+                        icon: Icons.work_rounded,
+                      ),
+                      ActionsWidget(
+                        name: 'Registrar Eventualidade',
+                        activities: ['horario', 'eventualidade'],
+                        icon: Icons.today_rounded,
+                      ),
+                      ActionsWidget(
+                        name: 'Registrar Atividade Física',
+                        activities: ['horario', 'exercício'],
+                        icon: Icons.fitness_center_rounded,
+                      ),
+                    ],
                   ),
-                  ActionsWidget(
-                    name: 'Registrar Alimentação',
-                    activities: ['horario', 'alimentos'],
+                ),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Divider(),
+            ),
+            InkWell(
+              onTap: (() => _changeVisibilityList()),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _textButton,
+                    style: const TextStyle(
+                        color: GlobalInfo.infoColor, fontSize: 14),
                   ),
-                  ActionsWidget(
-                    name: 'Registrar Atividades',
-                    activities: ['horario', 'atividade'],
-                  ),
-                  ActionsWidget(
-                    name: 'Registrar Eventualidade',
-                    activities: ['horario', 'eventualidade'],
-                  ),
-                  ActionsWidget(
-                    name: 'Registrar Atividade Física',
-                    activities: ['horario', 'exercício'],
-                  ),
+                  Icon(
+                    _iconButton,
+                    color: GlobalInfo.infoColor,
+                    size: 24,
+                  )
                 ],
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  void _changeVisibilityList() {
+    setState(() {
+      _visibleList = !_visibleList;
+
+      if (!_visibleList) {
+        _textButton = 'Ver mais  ';
+        _iconButton = Icons.expand_more_rounded;
+      } else {
+        _textButton = 'Ver menos  ';
+        _iconButton = Icons.expand_less_rounded;
+      }
+    });
   }
 }
 
@@ -167,11 +289,13 @@ class ActionsWidget extends StatelessWidget {
   const ActionsWidget({
     required this.name,
     required this.activities,
+    required this.icon,
     Key? key,
   }) : super(key: key);
 
   final String name;
   final List activities;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
@@ -186,24 +310,56 @@ class ActionsWidget extends StatelessWidget {
                 activities: activities,
               ),
             )),
-        child: Card(
-          color: GlobalInfo.secondaryColor,
-          child: Padding(
-            padding: const EdgeInsets.all(3),
-            child: Container(
-              color: Colors.white,
-              width: 100,
-              height: 100,
-              child: Center(
-                child: Text(
-                  name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: GlobalInfo.contrastColor,
-                    fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: GlobalInfo.tertiaryColor, width: 3)),
+            // width: 100,
+            height: 75,
+            child: Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            height: 35,
+                            width: 35,
+                            decoration: BoxDecoration(
+                                color: GlobalInfo.secondaryColor,
+                                borderRadius: BorderRadius.circular(50)),
+                            child: Icon(
+                              icon,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          name,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: GlobalInfo.contrastColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.arrow_forward_ios_rounded,
+                    color: GlobalInfo.grey,
+                    size: 18,
+                  ),
+                )
+              ],
             ),
           ),
         ),
